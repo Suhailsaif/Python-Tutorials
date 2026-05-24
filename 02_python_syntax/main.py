c413546ee19623f400eadc0670beedde9fd82a4d
Suhailsaif <m.suhail544321@gmail.com>
2026-05-24 09:49:53 +0530

    update code 24 may 2026

---
+++ 02_python_syntax/main.py
@@ -0,0 +1,10 @@
+print("Hellow suhail")
+print("Welcome to Python Course")
+print("This is the first line of code")
+print(3)
+
+a = 5
+if(a>3):
+    print("a is greater than 3")
+else:
+    print("a is less than or equal to 3")
